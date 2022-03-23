899534b2c2198a15a01b94f2ec3ba91e0371c039
L4RKjke <nik.trenikhin@gmail.com>
2022-03-24 01:05:36 +0500

    Task2
    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,19 +5,19 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        int value = 5;
-        float count = 3;
-        bool flag = false;
+        int peachesNumber = 5;
+        float halfOfPeaches = peachesNumber/2f;
+        bool isEmployed = false;
         char firstLetter = 'A';
-        byte num = 0xA;
+        byte num = 144;
         uint positiveNum = 4294967295;
         long bigNum = 9223372036854775807;
         ulong hugeNum = 18446744073709551615;
         short smallNum = 32767;
         string name = "Nikita";
-        Console.WriteLine(value);
-        Console.WriteLine(count);
-        Console.WriteLine(flag);
+        Console.WriteLine(peachesNumber);
+        Console.WriteLine(halfOfPeaches);
+        Console.WriteLine(isEmployed);
         Console.WriteLine(firstLetter);
         Console.WriteLine(num);
         Console.WriteLine(positiveNum);
