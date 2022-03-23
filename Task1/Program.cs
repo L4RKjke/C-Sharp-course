ae8c0a5d92ccb9aa5c3e367e606d59ce43e0ce29
L4RKjke <nik.trenikhin@gmail.com>
2022-03-24 00:45:12 +0500

    Task1
    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        int value = 5;
+        float count = 3;
+        bool flag = false;
+        char firstLetter = 'A';
+        byte num = 0xA;
+        uint positiveNum = 4294967295;
+        long bigNum = 9223372036854775807;
+        ulong hugeNum = 18446744073709551615;
+        short smallNum = 32767;
+        string name = "Nikita";
+        Console.WriteLine(value);
+        Console.WriteLine(count);
+        Console.WriteLine(flag);
+        Console.WriteLine(firstLetter);
+        Console.WriteLine(num);
+        Console.WriteLine(positiveNum);
+        Console.WriteLine(bigNum);
+        Console.WriteLine(hugeNum);
+        Console.WriteLine(smallNum);
+        Console.WriteLine(name);
+    }
+}
