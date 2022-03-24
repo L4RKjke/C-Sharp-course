54865d6889d9b913bb4f9897b6a9112ce105f7b9
L4RKjke <nik.trenikhin@gmail.com>
2022-03-24 17:26:19 +0500

    Taks6
    Taks6

---
+++ Taks6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int images = 52;
+        int imagesInRaw = 3;
+        int pictureRows = images / imagesInRaw;
+        int excessPictures = images % imagesInRaw;
+        Console.WriteLine($"Рядов с картинками: {pictureRows}, картинок сверх меры: {excessPictures}");
+    }
+}
