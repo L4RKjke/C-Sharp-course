e505bfd5bd3a787e179f2f1df31e850d85c24d64
L4RKjke <nik.trenikhin@gmail.com>
2022-03-24 16:59:49 +0500

    Task5
    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 10;
+        Console.WriteLine($"Изначально: a = {a}, b = {b}");
+        int c = a;
+        a = b;
+        b = c;
+        Console.WriteLine($"После перестановки: a = {a}, b = {b}");
+    }
+}
+
