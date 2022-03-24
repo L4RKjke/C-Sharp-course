7b8b559c522f2c2be026e4db7077978cdf0f2c79
L4RKjke <nik.trenikhin@gmail.com>
2022-03-24 18:08:30 +0500

    Task8
    Task8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -5,10 +5,11 @@ internal class Program
     static void Main(string[] args)
     {
         int receptionTime = 10;
+        int minutesInHour = 60;
         Console.WriteLine("Кол-во людей в очереди:");
         int peopleInQueues = Convert.ToInt32(Console.ReadLine());
-        int hours = (receptionTime * peopleInQueues) / 60;
-        int minutes = (receptionTime * peopleInQueues) % 60;
+        int hours = (receptionTime * peopleInQueues) / minutesInHour;
+        int minutes = (receptionTime * peopleInQueues) % minutesInHour;
         Console.WriteLine($"Вы должны отстоять в очереди {hours} часа и {minutes} минут.");
     }
 }
