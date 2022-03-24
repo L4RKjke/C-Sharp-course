5310cb795fa9e51ff600114859ebb5f9baf5d832
L4RKjke <nik.trenikhin@gmail.com>
2022-03-24 17:50:11 +0500

    Task7
    Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int gemsPrice = 5;
+        Console.WriteLine("Золота в кармане:");
+        int gold = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Сколько кристаллов хотите купить?");
+        int gems = Convert.ToInt32(Console.ReadLine());
+        int goldAfterDeal = gold - gemsPrice * gems;
+        Console.WriteLine($"золота после покупки: {goldAfterDeal}, кристаллов куплено: {gems}");
+    }
+}
