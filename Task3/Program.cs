eaf90d74a1df4e79b98f2929c4d768fc5ab7e979
L4RKjke <nik.trenikhin@gmail.com>
2022-03-24 16:04:17 +0500

    Task3
    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите свое имя:");
+        string name = Console.ReadLine();
+        Console.Write("Ваш рост:");
+        string height = Console.ReadLine();
+        Console.Write("Ваш возраст:");
+        string age = Console.ReadLine();
+        Console.Write("Ваш любимый фильм:");
+        string favoriteMovie = Console.ReadLine();
+        Console.WriteLine($"Вас зовут {name}, рост {height}см, вам {age}, любимый фильм {favoriteMovie}");
+    }
+}
