babfa6b9c0520764bac7edd6fae8625d2d2438bc
L4RKjke <nik.trenikhin@gmail.com>
2022-03-24 17:52:50 +0500

    Task7
    Task7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -9,7 +9,7 @@ internal class Program
         int gold = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Сколько кристаллов хотите купить?");
         int gems = Convert.ToInt32(Console.ReadLine());
-        int goldAfterDeal = gold - gemsPrice * gems;
-        Console.WriteLine($"золота после покупки: {goldAfterDeal}, кристаллов куплено: {gems}");
+        gold = gold - gemsPrice * gems;
+        Console.WriteLine($"золота после покупки: {gold}, кристаллов куплено: {gems}");
     }
 }
