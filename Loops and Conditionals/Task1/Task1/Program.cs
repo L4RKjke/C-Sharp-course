0d1aaa0687b3bc2eb83d3a2fe30a2ccc622a6676
L4RKjke <nik.trenikhin@gmail.com>
2022-04-02 04:34:56 +0500

    Task1
    Task1

---
+++ Loops and Conditionals/Task1/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое сообщение:");
+            string userMessage = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Сколько раз вы хотите продублировать свое сообщение?");
+            int numberOfMessages = Convert.ToInt32(Console.ReadLine());
+            int writtenMessages = 0;
+            while (writtenMessages < numberOfMessages)
+            {
+                writtenMessages++;
+                Console.WriteLine(userMessage);
+            }
+        }
+    }
+}
