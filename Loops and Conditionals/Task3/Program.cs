84837ba870acc6f5623eb56cd9a8935d3a3ad548
L4RKjke <nik.trenikhin@gmail.com>
2022-04-02 14:20:04 +0500

    Task3
    Task3

---
+++ Loops and Conditionals/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int orderСounter = 1;
+            Console.WriteLine("Для вывода полседовательности используем цикл 'for' так как: есть счетчик, и у последовательности  постоянный шаг");
+
+            for (int orderElement = 5; orderElement < 97; orderElement += 7)
+            {
+                Console.WriteLine(orderСounter + "st number of order: " + orderElement);
+                orderСounter += 1;
+            }
+
+        }
+    }
+}
