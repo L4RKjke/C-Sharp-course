1b6e145cb461e4a63ca6cb39b2f7de9aa40a4e8f
L4RKjke <nik.trenikhin@gmail.com>
2022-04-02 05:31:29 +0500

    Task1
    Task1

--- Loops and Conditionals/Task1/Program.cs
+++ Loops and Conditionals/Task1/Program.cs
@@ -14,10 +14,9 @@ namespace Task1
             string userMessage = Convert.ToString(Console.ReadLine());
             Console.WriteLine("Сколько раз вы хотите продублировать свое сообщение?");
             int numberOfMessages = Convert.ToInt32(Console.ReadLine());
-            int writtenMessages = 0;
-            while (writtenMessages < numberOfMessages)
-            {
-                writtenMessages++;
+
+            for (int i = 0; i < numberOfMessages; i++;)
+            { 
                 Console.WriteLine(userMessage);
             }
         }
