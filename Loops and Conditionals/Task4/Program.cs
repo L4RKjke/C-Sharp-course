c3c291a797b147cbf0e5150f56724d821636e83e
L4RKjke <nik.trenikhin@gmail.com>
2022-04-02 15:42:12 +0500

    Task4
    Task4

--- Loops and Conditionals/Task4/Program.cs
+++ Loops and Conditionals/Task4/Program.cs
@@ -12,16 +12,18 @@ namespace Task4
         {
             Random rand = new Random();
             int randomNumber = rand.Next(0, 101);
+            int sum = 0;
+            int firstDivisibilityCheck = 3;
+            int secondDivisibilityCheck = 5;
 
             for (int i = 0; i <= randomNumber; i ++)
             {
-                Console.WriteLine(i);
-                if (i%5 == 0 || i % 3 == 0)
+                if (i%firstDivisibilityCheck == 0 || i % secondDivisibilityCheck == 0)
                 {
-                    Console.WriteLine(i);
+                    sum += i;
                 }
-                
             }
+            Console.WriteLine(sum);
         }
     }
 }
