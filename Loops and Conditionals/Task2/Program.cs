5009bd98d33b7a405d8cbf6acdb6a5e9f692a69f
L4RKjke <nik.trenikhin@gmail.com>
2022-04-02 05:11:05 +0500

    Task2
    Task2

---
+++ Loops and Conditionals/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Перечислите реки России, которые вы знаете:");
+            string stopWord = "exit";
+
+            while (true)
+            {
+                string word = Convert.ToString(Console.ReadLine());
+
+                if (word == stopWord)
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
