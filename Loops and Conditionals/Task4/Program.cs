cf2f9a16107f5c8ff1271cc3926c00fe14df333f
L4RKjke <nik.trenikhin@gmail.com>
2022-04-02 14:34:10 +0500

    Task4
    Task4

---
+++ Loops and Conditionals/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next(0, 101);
+
+            for (int i = 0; i <= randomNumber; i ++)
+            {
+                Console.WriteLine(i);
+                if (i%5 == 0 || i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+        }
+    }
+}
