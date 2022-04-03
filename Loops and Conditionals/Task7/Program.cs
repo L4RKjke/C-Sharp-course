16bb4e77b8908a3a8dabae203ab2506bd57f5f29
L4RKjke <nik.trenikhin@gmail.com>
2022-04-03 21:16:23 +0500

    Task7
    Task7

---
+++ Loops and Conditionals/Task7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите любой символ:");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < name.Length + 2; j++)
+                {
+                    if (i == 1)
+                    {
+                        Console.Write(symbol + name + symbol);
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write(symbol);
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
