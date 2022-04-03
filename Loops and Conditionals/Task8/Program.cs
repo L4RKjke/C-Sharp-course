f3b98e370b79cc3df227ae264f3118540c2c80d0
L4RKjke <nik.trenikhin@gmail.com>
2022-04-03 21:54:03 +0500

    Task8
    Task8

---
+++ Loops and Conditionals/Task8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "12345678";
+            bool isCorrect = false;
+            int attemptsLeft = 3;
+
+            while (isCorrect == false)
+            {
+                Console.WriteLine("Введите пароль:");
+                string enteredPassword = Console.ReadLine();
+                attemptsLeft--;
+
+                if (attemptsLeft == 0)
+                {
+                    Console.WriteLine("Попыток больше нет!");
+                    break;
+                }
+                if (enteredPassword == password)
+                {
+                    Console.WriteLine("safe password: 83742803");
+                    isCorrect = true;
+                }
+                else
+                {
+                    Console.WriteLine("Пароль невверный! Попыток осталось: " + attemptsLeft);
+                }
+            }
+            
+        }
+    }
+}
