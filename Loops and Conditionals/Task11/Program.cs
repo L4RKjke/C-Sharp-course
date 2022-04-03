ca80faafb9a60c31386d8773da94a187ff90ae85
L4RKjke <nik.trenikhin@gmail.com>
2022-04-04 03:30:04 +0500

    Task11
    Task11

---
+++ Loops and Conditionals/Task11/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "((())(()))";
+            int bracketBalance = 0;
+            bool error = false;
+            int maxBracketDeep = 0;
+
+            foreach (var symbol in testString)
+            {
+                if (bracketBalance < 0)
+                {
+                    error = true;
+                }
+                else
+                {
+                    if (Convert.ToString(symbol) == "(")
+                    {
+                        bracketBalance += 1;
+                    }
+
+                    if (Convert.ToString(symbol) == ")")
+                    {
+                        bracketBalance -= 1;
+                    }
+
+                    if (maxBracketDeep < bracketBalance)
+                    {
+                        maxBracketDeep = bracketBalance;
+                    }                  
+                }
+            }
+            if (bracketBalance == 0 & error == false)
+            {
+                Console.WriteLine("Ok!");
+                Console.WriteLine("Максимальная глубина вложенности скобок: " + maxBracketDeep);
+            }
+            else
+            {
+                Console.WriteLine("bracketError!");
+            }
+        }
+    }
+}
