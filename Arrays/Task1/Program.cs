d0031b93eaeaaaa2ee539bb6223a7b2d1617f630
L4RKjke <nik.trenikhin@gmail.com>
2022-04-05 03:45:45 +0500

    Arrays
    Arrays

---
+++ Arrays/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumOfsecondString = 0;
+            int multiplyOfFirstColumn = 1;
+            int[,] givenArray =
+            {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+
+            for (int i = 0; i < givenArray.GetLength(0); i++)
+            {
+                multiplyOfFirstColumn *= givenArray[i, 0];
+
+                for (int j = 0; j < givenArray.GetLength(1); j++)
+                {
+                    if (i == 1)
+                    {
+                        sumOfsecondString += givenArray[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("sum of second string: " + sumOfsecondString);
+            Console.WriteLine("multiply of first column: " + multiplyOfFirstColumn);
+        }
+    }
+}
---
+++ Arrays/Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] givenArray = new int[10, 10];
+            int maxElement = 0;
+            Random random = new Random();
+
+            for (int k = 0; k < 2; k++)
+            {
+                for (int i = 0; i < givenArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < givenArray.GetLength(1); j++)
+                    {
+                        if (k == 0)
+                        {
+                            givenArray[i, j] = random.Next(1, 100);
+                            Console.Write("{0,4}", givenArray[i, j]);
+
+                            if (maxElement < givenArray[i, j])
+                            {
+                                maxElement = givenArray[i, j];
+                            }
+                        }
+                        else
+                        {
+                            if (givenArray[i, j] == maxElement)
+                            {
+                                givenArray[i, j] = 0;
+                                Console.Write("{0,4}", givenArray[i, j]);
+                            }
+                            else
+                            {
+                                givenArray[i, j] = givenArray[i, j];
+                                Console.Write("{0,4}", givenArray[i, j]);
+                            }
+                        }
+                    }
+                    Console.WriteLine("\n");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("max element: " + maxElement);
+        }
+    }
+}
