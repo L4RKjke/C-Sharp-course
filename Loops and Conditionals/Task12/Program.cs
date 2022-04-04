6256390abfeabe4d382cadef45df21ee5c3a778b
L4RKjke <nik.trenikhin@gmail.com>
2022-04-04 13:18:07 +0500

    Task12
    Task12

--- Loops and Conditionals/Task12/Program.cs
+++ Loops and Conditionals/Task12/Program.cs
@@ -17,86 +17,92 @@ namespace Task12
             int bossMaxDamage = 40;
             int bossMinDamage = 20;
             float userArmor = 0.6f;
-            bool isRepeat = true;
             bool isSpiritCalled = false;
-            int damageBuff = 2;
+            int damageBuff = 3;
             int userHealthBuff = 1;
-            int userSpiritBuff = 3;
+            int maxPercent = 100;
+            int minPercent = 0;
             Random random = new Random();
+            int skipTurnСhance = 40;
+            int randomNumberInPercentInterval = random.Next(minPercent + 1, maxPercent + 1);
             Console.WriteLine("Способности: ");
-            Console.WriteLine("1) Простой удар: 25 - 40 урона");
-            Console.WriteLine("2) Супер удар (двойной урон, вероятность пропуска хода 50%)");
+            Console.WriteLine("1) Простой удар: минимальный урон: 25, максимальный урон: 40");
+            Console.WriteLine("2) Супер удар (тройной урон, вероятность пропуска хода 50%)");
             Console.WriteLine("3) Призыв духа (вы пропускаете 1 ход)");
             Console.WriteLine("4) Слияние с духом: повышает урон на 200 %, повышает здоровье на 100%. Условие: вы призывали духа");
 
-            while (isRepeat)
+            while (userHealth > 0 & bossHealth > 0)
             {
+                bool skipTurn = false;
                 Console.WriteLine("Boss hp: " + bossHealth);
                 Console.WriteLine("User hp: " + userHealth);
                 Console.WriteLine("Ваш ход: ");
-                int UserTurn = Convert.ToInt32(Console.ReadLine()); 
-                switch (UserTurn)
-                {
-                    case 1:
-                        userHealth -= (random.Next(bossMinDamage, bossMaxDamage) * userArmor);
-                        bossHealth -= random.Next(userMinDamage, userMaxDamage);
-                        break;
-                    case 2:
-                        int skipTurnСhance = random.Next(0, 2);
+                string userTurn = Console.ReadLine();
+                int temporaryDamageBuff = 1;
 
-                        if (skipTurnСhance == 0)
+                switch (userTurn)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        if (randomNumberInPercentInterval <= skipTurnСhance)
                         {
-                            userHealth -= (random.Next(bossMinDamage, bossMaxDamage) * userArmor);
-                            bossHealth -= random.Next(userMinDamage * damageBuff, userMaxDamage * damageBuff);
-                        }
-                        else
-                        {
-                            userHealth -= (random.Next(bossMinDamage, bossMaxDamage) * userArmor);
+                            skipTurn = true;
+                            bossHealth -= random.Next(userMinDamage, userMaxDamage) * temporaryDamageBuff;
                         }
                         break;
-                    case 3:
+                    case "3":
 
                         if (!isSpiritCalled)
                         {
+                            skipTurn = true;
                             isSpiritCalled = true;
-                            userHealth -= (random.Next(bossMinDamage, bossMaxDamage) * userArmor);
                         }
+
                         else
                         {
                             Console.WriteLine("Вы уже призывали духа!");
-                            userHealth -= (random.Next(bossMinDamage, bossMaxDamage) * userArmor);
-                            bossHealth -= random.Next(userMinDamage, userMaxDamage);
                         }
                         break;
-                    case 4:
+                    case "4":
 
                         if (isSpiritCalled)
                         {
-                            userMinDamage = userSpiritBuff * userMinDamage;
-                            userMaxDamage = userSpiritBuff * userMaxDamage;
-                            bossHealth -= random.Next(userMinDamage, userMaxDamage);
                             userHealth = userHealth * userHealthBuff;
-                            userHealth -= (random.Next(bossMinDamage, bossMaxDamage) * userArmor);
+                            userMinDamage = damageBuff * userMinDamage;
+                            userMaxDamage = damageBuff * userMinDamage;
                             isSpiritCalled = false;
                         }
                         else
                         {
                             Console.WriteLine("Вы не можете слится с духом!");
-                            bossHealth -= random.Next(userMinDamage, userMaxDamage);
-                            userHealth -= (random.Next(bossMinDamage, bossMaxDamage) * userArmor);
                         }
                         break;
                 }
-                if (userHealth <= 0)
+                if (skipTurn)
+                {
+                    userHealth -= (random.Next(bossMinDamage, bossMaxDamage) * userArmor);
+                }
+                else
+                {
+                    userHealth -= (random.Next(bossMinDamage, bossMaxDamage) * userArmor);
+                    bossHealth -= random.Next(userMinDamage, userMaxDamage);
+                }
+            }
+            if (userHealth <= 0)
+            {
+                if (bossHealth <= 0)
+                {
+                    Console.WriteLine("Бой закончился ничьей");
+                }
+                else
                 {
                     Console.WriteLine("Бой окончен. Вы проиграли!");
-                    isRepeat = false;
                 }
-                else if (bossHealth <= 0)
-                {
-                    Console.WriteLine("Бой окончен. Вы победили!");
-                    isRepeat = false;
-                }
+            }
+            else
+            {
+                Console.WriteLine("Бой окончен. Вы победили!");
             }
         }
     }
