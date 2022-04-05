f0655992849fe21b39dd6a59f7e6e2212258808f
L4RKjke <nik.trenikhin@gmail.com>
2022-04-06 03:15:39 +0500

    Task6
    Task6

---
+++ Arrays/Task6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomTxt = Console.ReadLine();
+            string[] textString = randomTxt.Split(' ');
+
+            foreach (string word in textString)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
