052bf523c7bd48ba1a6a2d616e23526e1ea8060a
L4RKjke <nik.trenikhin@gmail.com>
2022-04-07 00:05:42 +0500

    Task7 (Bubble-sort)
    Task7 (Bubble-sort)

---
+++ Arrays/Task7 (Bubble-sort)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7__Bubble_sort_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 1, 43, 2, 8, 41, 43, 1, 6, 1};
+            int tempValue = 0;
+            bool isSorted = true;
+            bool endSort = false;
+            Console.WriteLine("Bubble - sort: \n");
+
+            while (endSort == false)
+            {
+                isSorted = true;
+
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        tempValue = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = tempValue;
+                        isSorted = false;
+                    }
+                }
+                if (isSorted)
+                {
+                    endSort = true;
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
