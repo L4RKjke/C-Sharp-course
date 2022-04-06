75e2e31e5ba6619e3b0fd476172b6c87747b417b
L4RKjke <nik.trenikhin@gmail.com>
2022-04-07 00:15:39 +0500

    Task8
    Task8

---
+++ Arrays/Task8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110 };
+            Console.WriteLine("Введите шаг сдвига n: \n");
+            int moveStep = Convert.ToInt32(Console.ReadLine());
+            int tempValue = 0;
+            Console.WriteLine("");
+
+            for (int j = 0; j < moveStep; j++)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    tempValue = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = tempValue;
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
