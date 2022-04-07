b73d65b19f878f073a99f99424d751ad8dbdff88
L4RKjke <nik.trenikhin@gmail.com>
2022-04-08 04:31:29 +0500

    Task3
    Task3

---
+++ Functions/Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IntCheck();
+        }
+        static void IntCheck()
+        {
+            bool isClose = false;
+
+            while (isClose == false)
+            {
+                string enteredNumber = Console.ReadLine();
+
+                if (int.TryParse(enteredNumber, out int result))
+                {
+                    Console.WriteLine(enteredNumber);
+                    isClose = true;
+                }
+                else
+                {
+                    Console.WriteLine("Попробуйте еще раз:");
+                }
+            }
+        }
+    }
+}
