597c5e01294449de2fe89f56d9f9425b98b9d28c
L4RKjke <nik.trenikhin@gmail.com>
2022-04-07 11:41:58 +0500

    Task5
    Task5

--- Functions/Task5/Program.cs
+++ Functions/Task5/Program.cs
@@ -12,47 +12,30 @@ namespace Task5
         {
             int[] numberArray = { 10, 20, 30, 40, 50, 60, 70 };
 
-            Shuffle(ref numberArray);
+            Shuffle(numberArray);   
 
             for (int i = 0; i < numberArray.Length; i++)
             {
                 Console.WriteLine(numberArray[i]);
             }
         }
-        static void Shuffle(ref int[] array)
+        static void Shuffle(int[] array)
         {
-            int[] randomArray = new int[array.Length];
             int randomNumber = 0;
             int tempValue = 0;
             Random random = new Random();
 
-            for (int i = 0; i < array.Length; i++)
+            for (int j = 0; j < random.Next(0, array.Length); j++)
             {
-                while (Contain(randomArray, randomNumber))
-                {
-                    randomNumber = random.Next(0, array.Length + 1);
-                }
-                randomArray[i] = randomNumber;
-            }
+                randomNumber = random.Next(0, array.Length);
 
-            for (int i = 0; i < array.Length; i++)
-            {
-                tempValue = array[i];
-                array[i] = array[randomArray[i] - 1];
-                array[randomArray[i] - 1] = tempValue;
-            }
-        }
-
-        static bool Contain(int[] array, int num)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] == num)
+                for (int i = 0; i < array.Length - 1; i++)
                 {
-                    return true;
+                    tempValue = array[i];
+                    array[i] = array[randomNumber + 1];
+                    array[randomNumber + 1] = tempValue;
                 }
             }
-            return false;
         }
     }
 }
