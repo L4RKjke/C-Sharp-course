067108ad2fe665bbd807494e3f97ebc01de22204
L4RKjke <nik.trenikhin@gmail.com>
2022-04-08 18:06:56 +0500

    Task2
    Task2

---
+++ Functions/Task2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.SetCursorPosition(0, 5);
+                int maxPercent = 100;
+                Console.WriteLine("Процент маны:");
+                int manaPercent = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Процент здоровья:");
+                int healthPercent = Convert.ToInt32(Console.ReadLine());
+                DrawBar(manaPercent, maxPercent, ConsoleColor.Red, 0, '_');
+                DrawBar(healthPercent, maxPercent, ConsoleColor.Blue, 1, '_');
+                Console.SetCursorPosition(0, 9);
+                Console.WriteLine("Для продолжения нажмите любую кнопку.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+        }
+
+        static void DrawBar(int givenPercent, int maxPercent, ConsoleColor color, int position, char symbol = ' ')
+        {
+            Console.SetCursorPosition(0, position);
+            ConsoleColor barColor = Console.BackgroundColor; 
+            if (givenPercent <= maxPercent & givenPercent >= 0)
+            {
+                Console.Write("|");
+                for (int i = 0; i < maxPercent / 5; i++)
+                {
+                    if (i < givenPercent / 5)
+                    {
+                        Console.BackgroundColor = color;
+                        Console.Write('#');
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ResetColor();
+                        Console.Write(symbol);
+                    }
+                }
+                Console.Write("|");
+            }
+            else
+            {
+                Console.WriteLine("Введите процент от 0 до 100!");
+            }
+        }
+    }
+}
