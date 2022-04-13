9e8656806afb2d3a642e1a2df0fcce3b0b66558b
L4RKjke <nik.trenikhin@gmail.com>
2022-04-13 22:50:39 +0500

    Task2
    Task2

--- Collections/Task2/Program.cs
+++ Collections/Task2/Program.cs
@@ -10,32 +10,13 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int randomProduct;
             int shopBalance = 0;
-            List<string> clientsNames = new List<string>() { "Никита", "Максим", "Саша" };
-            Queue<string> clients = new Queue<string>(clientsNames);
-            Dictionary<string, int> gods = new Dictionary<string, int>()
-            {
-                { "Сапоги", 700 },
-                { "Перчатки", 80 },
-                { "Каска", 2000 },
-                { "Клей", 60 },
+            List<int> clientsNames = new List<int>() { 700, 80, 2000, 60 };
+            Queue<int> clients = new Queue<int>(clientsNames);
 
-            };
-            Dictionary<int, string> productOrder = new Dictionary<int, string>()
-            {
-                { 1,  "Сапоги"},
-                { 2,  "Перчатки"},
-                { 3,  "Каска"},
-                { 4,  "Клей"},
-
-            };
             while (clients.Count > 0)
             {
-                randomProduct = random.Next(1, 5);
-                shopBalance += gods[productOrder[randomProduct]];
-                Console.WriteLine($"{clients.Peek()} купил: '{productOrder[randomProduct]}'");
+                shopBalance += clients.Peek();
                 clients.Dequeue();
                 Console.WriteLine($"Баланс магазина: {shopBalance}");
                 Console.ReadKey(true);
