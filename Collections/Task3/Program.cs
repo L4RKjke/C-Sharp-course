a165f1daa1928423f048a33dee7339deb803bc5c
L4RKjke <nik.trenikhin@gmail.com>
2022-04-14 00:32:50 +0500

    Task3
    Task3

---
+++ Collections/Task3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isClose = false;
+            List<int> numbers = new List<int>();
+            Console.WriteLine("Суммирование чисел. 'exit'- выход из программы, 'sum' - вывод суммы введенных чисел \n");
+
+            while (isClose == false)
+            {
+                Console.WriteLine("Ввод:");
+                string inputValue = Console.ReadLine();
+
+                if (int.TryParse(inputValue, out int result) == true)
+                {
+                    numbers.Add(result);
+                }
+                else
+                {
+                    switch (inputValue)
+                    {
+                        case "exit":
+                            isClose = true;
+                            break;
+
+                        case "sum":
+                            int sum;
+                            sum = numbers.Sum();
+                            Console.WriteLine(sum);
+                            numbers.RemoveAll(numbers.Contains);
+                            Console.WriteLine("Нажмите любую кнопку, чтобы продолжить");
+                            Console.ReadKey(true);
+                            Console.Clear();
+                            break;
+
+                        default:
+                            Console.WriteLine("Ошибка ввода!");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
