27b22c622fae8f25abc9be0c36da362aea48c893
L4RKjke <nik.trenikhin@gmail.com>
2022-04-19 01:57:52 +0500

    Task2
    Task2

---
+++ OOP/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player();
+            Renderer drawPlayer1 = new Renderer();
+
+            drawPlayer1.RenderPosition(player1.PositionX, player1.PositionY);
+        }
+    }
+
+    class Player
+    {
+        public int PositionX
+        {
+            get;
+            private set;
+        } = 5;
+        public int PositionY
+        {
+            get;
+            private set;
+        } = 2;
+    }
+
+    class Renderer
+    {
+        public void RenderPosition(int PositionX, int PositionY)
+        {
+            Console.SetCursorPosition(PositionX, PositionY);
+            Console.WriteLine("/ (-)(-) \\");
+        }
+    }
+}
