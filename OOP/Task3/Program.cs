bd183d98ac5e01062f2e0133e55f517b88e0d40e
L4RKjke <nik.trenikhin@gmail.com>
2022-04-19 12:06:25 +0500

    Task3
    Реализовать базу данных игроков и методы для работы с ней.
    У игрока может быть уникальный номер, ник, уровень, флаг – забанен ли он(флаг - bool).
    Реализовать возможность добавления игрока, бана игрока по уникальный номеру, разбана игрока по уникальный номеру и удаление игрока.
    Создание самой БД не требуется, задание выполняется инструментами, которые вы уже изучили в рамках курса. Но нужен класс, который содержит игроков и её можно назвать "База данных".

---
+++ OOP/Task3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player("Bob", 12, false);
+            Player player2 = new Player("Max", 6, true);
+            DataBase base1 = new DataBase();
+            base1.AddPlayer(player1.WriteInfo());
+            base1.AddPlayer(player2.WriteInfo());
+            base1.ShowInfo();
+            base1.BanPlayer(0);
+            base1.UnbanPlayer(1);
+            base1.ShowInfo();
+        }
+    }
+
+    class DataBase
+    {
+        private Dictionary<int, Dictionary<string, string>> _playersData = new Dictionary<int, Dictionary<string, string>>();
+        private int _userId;
+
+        public void AddPlayer(Dictionary<string, string> userInfo)
+        {
+            _playersData[_userId] = userInfo;
+            _userId++;
+        }
+
+        public void RemovePlayer(int userId)
+        {
+            _playersData.Remove(userId);
+        }
+
+        public void BanPlayer(int userId)
+        {
+            _playersData[userId]["is banned"] = "True";
+        }
+
+        public void UnbanPlayer(int userId)
+        {
+            _playersData[userId]["is banned"] = "False";
+        }
+
+        public void ShowInfo()
+        {
+            foreach (var id in _playersData.Keys)
+            {
+                Console.WriteLine($"id: {id}; name: {_playersData[id]["name"]}; level: {_playersData[id]["level"]}; banned: {_playersData[id]["is banned"]}");
+            }
+        }
+    }
+
+    class Player
+    {
+        private string _name;
+        private int _level;
+        private bool _isBanned = false;
+
+        public Player(string name, int level, bool isBanned)
+        {
+            _name = name;
+            _level = level;
+            _isBanned = isBanned;
+        }
+
+        public Dictionary<string, string> WriteInfo()
+        {
+            Dictionary<string, string> userData = new Dictionary<string, string>() { { "name", _name }, { "level", Convert.ToString(_level) }, { "is banned", Convert.ToString(_isBanned) } };
+            return userData;
+        }
+    }
+}
