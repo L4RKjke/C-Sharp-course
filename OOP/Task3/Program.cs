d9f0f7eb44301f90b279d13977c7f153c842173c
L4RKjke <nik.trenikhin@gmail.com>
2022-04-19 16:55:51 +0500

    Task3
    Реализовать базу данных игроков и методы для работы с ней.
    У игрока может быть уникальный номер, ник, уровень, флаг – забанен ли он(флаг - bool).
    Реализовать возможность добавления игрока, бана игрока по уникальный номеру, разбана игрока по уникальный номеру и удаление игрока.
    Создание самой БД не требуется, задание выполняется инструментами, которые вы уже изучили в рамках курса. Но нужен класс, который содержит игроков и её можно назвать "База данных".

--- OOP/Task3/Program.cs
+++ OOP/Task3/Program.cs
@@ -7,50 +7,41 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            Player player1 = new Player("Bob", 12, false);
-            Player player2 = new Player("Max", 6, true);
-            DataBase base1 = new DataBase();
-            base1.AddPlayer(player1.WriteInfo());
-            base1.AddPlayer(player2.WriteInfo());
-            base1.ShowInfo();
-            base1.BanPlayer(0);
-            base1.UnbanPlayer(1);
-            base1.ShowInfo();
+            DataBase data = new DataBase();
+            data.AddPlayer("Bob", 12, false);
+            data.AddPlayer("Max", 6, true);
+            data.BanPlayer(0);
+            data.UnbanPlayer(1);;
+            data.RemovePlayer(1); ;
         }
     }
 
     class DataBase
     {
-        private Dictionary<int, Dictionary<string, string>> _playersData = new Dictionary<int, Dictionary<string, string>>();
+        private Dictionary<int, Player> _playersData = new Dictionary<int, Player>();
         private int _userId;
 
-        public void AddPlayer(Dictionary<string, string> userInfo)
+        public void AddPlayer(string name, int level, bool isBanned)
         {
-            _playersData[_userId] = userInfo;
+            _playersData[_userId] = (new Player(name, level, isBanned));
             _userId++;
         }
 
         public void RemovePlayer(int userId)
         {
             _playersData.Remove(userId);
+
         }
 
         public void BanPlayer(int userId)
         {
-            _playersData[userId]["is banned"] = "True";
+            _playersData[userId].IsBanned = true;
+
         }
 
         public void UnbanPlayer(int userId)
         {
-            _playersData[userId]["is banned"] = "False";
-        }
-
-        public void ShowInfo()
-        {
-            foreach (var id in _playersData.Keys)
-            {
-                Console.WriteLine($"id: {id}; name: {_playersData[id]["name"]}; level: {_playersData[id]["level"]}; banned: {_playersData[id]["is banned"]}");
-            }
+            _playersData[userId].IsBanned = false;
         }
     }
 
@@ -58,19 +49,13 @@ namespace Task3
     {
         private string _name;
         private int _level;
-        private bool _isBanned = false;
+        public bool IsBanned { private get; set; }
 
         public Player(string name, int level, bool isBanned)
         {
             _name = name;
             _level = level;
-            _isBanned = isBanned;
-        }
-
-        public Dictionary<string, string> WriteInfo()
-        {
-            Dictionary<string, string> userData = new Dictionary<string, string>() { { "name", _name }, { "level", Convert.ToString(_level) }, { "is banned", Convert.ToString(_isBanned) } };
-            return userData;
+            IsBanned = isBanned;
         }
     }
 }
