3a9a245dab02593f705d56e2bc61e9b4ee715f3a
L4RKjke <nik.trenikhin@gmail.com>
2022-04-19 23:53:25 +0500

    Task3
    Реализовать базу данных игроков и методы для работы с ней.
    У игрока может быть уникальный номер, ник, уровень, флаг – забанен ли он(флаг - bool).
    Реализовать возможность добавления игрока, бана игрока по уникальный номеру, разбана игрока по уникальный номеру и удаление игрока.
    Создание самой БД не требуется, задание выполняется инструментами, которые вы уже изучили в рамках курса. Но нужен класс, который содержит игроков и её можно назвать "База данных".

--- OOP/Task3/Program.cs
+++ OOP/Task3/Program.cs
@@ -7,12 +7,121 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            DataBase data = new DataBase();
-            data.AddPlayer("Bob", 12, false);
-            data.AddPlayer("Max", 6, true);
-            data.BanPlayer(0);
-            data.UnbanPlayer(1);;
-            data.RemovePlayer(1); ;
+            GameMenager meneger = new GameMenager();
+            meneger.StartMenu();
+        }
+    }
+
+    class GameMenager
+    {
+        private bool _isClose = false;
+        private DataBase _data = new DataBase();
+
+        public void StartMenu()
+        {
+            while (_isClose == false)
+            {
+                Console.Clear();
+                DrawMenu();
+                SelectComand();
+                Console.WriteLine("Нажмите любую кнопку, чтобы продолжить...");
+                Console.ReadKey(true);
+            }
+        }
+
+        public void SelectComand()
+        {
+            char menuComand = Convert.ToChar(Console.ReadKey(true).Key);
+            Console.Clear();
+
+            switch (menuComand)
+            {
+                case '1':
+                    AddNewUser();
+                    break;
+
+                case '2':
+                    DeleteUser();
+                    break;
+
+                case '3':
+                    BanUser();
+                    break;
+
+                case '4':
+                    UnbanUser();
+                    break;
+
+                case '5':
+                    ShowAllPlayers();
+                    break;
+
+                case '0':
+                    _isClose = true;
+                    Console.Clear();
+                    break;
+
+                default:
+                    Console.WriteLine("Неверный номер команды! Нажмите любую кнопку, чтобы продолжить...");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+
+        public void DrawMenu()
+        {
+        Console.WriteLine("Меню\n");
+        Console.WriteLine("1 - Добавить игрока\n");
+        Console.WriteLine("2 - Удалить игрока\n");
+        Console.WriteLine("3 - Забанить игрока\n");
+        Console.WriteLine("4 - Разбанить игрока\n");
+        Console.WriteLine("5 - Вывести список всех игроков\n");
+        Console.WriteLine("0 - Выход\n");
+        }
+
+        public void AddNewUser()
+        {
+            Console.WriteLine("Введите данные для добавления нового игрока: \n");
+            Console.Write("Никнейм: ");
+            string name = Console.ReadLine();
+            Console.Write("Уровень игрока: ");
+
+            if (int.TryParse(Console.ReadLine(), out int result) && result >= 0)
+            {
+                Console.Write("Статус игрока (true - забанен, false - не забанен): ");
+                bool banStatus = (Convert.ToBoolean(Console.ReadLine()));
+                _data.AddPlayer(name, result, banStatus);
+            }        
+            else
+            {
+                Console.WriteLine("Уровень должен быть целым числом больше 0!");
+            }
+        }
+
+        public void DeleteUser()
+        {
+            Console.Write("Введите id для игрока, которого хотите удалить: ");
+            int userId = Convert.ToInt32(Console.ReadLine());
+            _data.RemovePlayer(userId);
+        }
+
+        public void BanUser()
+        {
+            Console.Write("Введите id для игрока, которого хотите забанить: ");
+            int userId = Convert.ToInt32(Console.ReadLine());
+            _data.BanPlayer(userId);
+        }
+
+        public void UnbanUser()
+        {
+            Console.Write("Введите id для игрока, которого хотите разбанить: ");
+            int userId = Convert.ToInt32(Console.ReadLine());
+            _data.UnbanPlayer(userId);
+        }
+
+        public void ShowAllPlayers()
+        {
+            _data.ShowInfo();
         }
     }
 
@@ -35,13 +144,20 @@ namespace Task3
 
         public void BanPlayer(int userId)
         {
-            _playersData[userId].IsBanned = true;
-
+            _playersData[userId].BanUser(true);
         }
 
         public void UnbanPlayer(int userId)
         {
-            _playersData[userId].IsBanned = false;
+            _playersData[userId].BanUser(false);
+        }
+
+        public void ShowInfo ()
+        {
+            foreach (var id in _playersData.Keys)
+            {
+                Console.WriteLine($"id: {id}, { _playersData[id].ShowPlayerInfo()}");
+            }
         }
     }
 
@@ -49,13 +165,26 @@ namespace Task3
     {
         private string _name;
         private int _level;
-        public bool IsBanned { private get; set; }
+        private bool _isBanned;
 
         public Player(string name, int level, bool isBanned)
         {
             _name = name;
             _level = level;
-            IsBanned = isBanned;
+            _isBanned = isBanned;
+        }
+
+        public void BanUser(bool status)
+        {
+            if (status) 
+                _isBanned = true;
+            else
+                _isBanned = false;
+        }
+
+        public string ShowPlayerInfo()
+        {
+            return ($"name: {_name}, level: {_level}, ban status: {_isBanned}");
         }
     }
 }
