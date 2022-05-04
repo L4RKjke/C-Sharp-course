f3d14630efcb3b0c5dbc15cfc6791d8a8b822539
L4RKjke <nik.trenikhin@gmail.com>
2022-05-04 16:11:08 +0500

    Task4

---
+++ LINQ/Task4/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBase dataBase = new DataBase();
+            Console.WriteLine("Топ 3 игрока по силе:\n");
+            dataBase.ShowTopByStrenght();
+            Console.WriteLine("\nТоп 3 по игрока уровню:\n");
+            dataBase.ShowTopByLevel();
+        }
+    }
+
+    class DataBase
+    {
+        private List<Player> _players;
+
+        public DataBase()
+        {
+            _players = new List<Player>() 
+            {
+                new Player("player1", 2, 20),
+                new Player("player2", 4, 10),
+                new Player("player3", 14, 10),
+                new Player("player4", 26, 200),
+                new Player("player5", 4, 23),
+                new Player("player6", 50, 100),
+                new Player("player7", 1, 10),
+                new Player("player8", 54, 5000),
+                new Player("player9", 12, 12),
+                new Player("player10", 80, 3000),
+            };
+        }
+
+        public void ShowTopByLevel()
+        {
+            var topLevelPlayers = _players.OrderByDescending(player => player.Level).Take(3);
+            ShowPlayers(topLevelPlayers);
+        }
+
+        public void ShowTopByStrenght()
+        {
+            var topStrenghtPlayers = _players.OrderByDescending(player => player.Strenght).Take(3); ;
+            ShowPlayers(topStrenghtPlayers);
+        }
+
+        private void ShowPlayers(IEnumerable<Player> selectedPlayers) 
+        {
+            foreach (var player in selectedPlayers)
+            {
+                Console.WriteLine($"{player.Name}, уровень: {player.Level}, сила: {player.Strenght}");
+            }
+        }
+    }
+
+    class Player
+    {
+        public string Name { get; private set; }
+        public int Level { get; private set; }
+        public int Strenght { get; private set; }
+
+        public Player(string name, int level, int strenght)
+        {
+            Name = name; 
+            Level = level;
+            Strenght = strenght;
+        }
+    }
+}
